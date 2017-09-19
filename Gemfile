920e62af16ea8987a46dc7c7a37fe9dce53fc5e8
mcpherson01 <mcpherson@dr.com>
2017-09-20 05:48:25 +0800

    initial commit

--- Gemfile
+++ Gemfile
@@ -52,3 +52,7 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+gem 'foundation-rails'
+gem 'administrate'
+gem 'impressionist'
+gem 'rails_db'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    resources :movies
+
+    root to: "movies#index"
+  end
+
   resources :movies, only: [:show, :index]
   root 'movies#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
