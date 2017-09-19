fa59c9bb2f403ac2d97d6a4a8a574cf2bc8b2777
mcpherson01 <mcpherson@dr.com>
2017-09-20 02:49:01 +0800

    update commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  resources :movies, only: [:show, :index]
+  root 'movies#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
