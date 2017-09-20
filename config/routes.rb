a0be403c2e3547c9792fe22b7b21cfa3257349de
mcpherson01 <mcpherson@dr.com>
2017-09-20 09:15:10 +0800

    initial

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+
+  get 'carts/show'
+
+devise_for :users, path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
+
   namespace :admin do
     resources :movies
 
